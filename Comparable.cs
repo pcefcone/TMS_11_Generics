4ab580dc3f4dd3d00cc27656c614aee5214a14e4
Yaroslav <yaroslav.kerez@gmail.com>
2024-08-12 18:00:39 +0200

    ver 3

---
+++ Comparable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS_11_Generics
+{
+    public class Comparable<T, U> : IComparable<Comparable<T, U>> where U : IComparable<T> where T : IComparable<U>
+    {
+        public T firstValue;
+        public U secondValue;
+
+        public Comparable(T firstValue, U secondValue)
+        {
+            this.firstValue = firstValue;
+            this.secondValue = secondValue;
+        }
+
+        public int CompareTo(Comparable<T, U>? other)
+        {
+            if (other == null)
+            {
+                return 1;
+            }
+            int firstComparsion = firstValue.CompareTo(other.firstValue);
+            if (firstComparsion != 0)
+            {
+                return firstComparsion;
+            }
+            return secondValue.CompareTo(other.secondValue);
+        }
+    }
+
+
+
+}
+
--- Program.cs
+++ Program.cs
@@ -1,7 +1,22 @@
-﻿internal class Program
+﻿using TMS_11_Generics;
+
+internal class Program
 {
+    internal class Pair<A, B>
+    {
+        public A firstVal;
+        public B secondVal;
+        public Pair(A firstValue, B secondValue)
+        {
+            this.firstVal = firstValue;
+            this.secondVal = secondValue;
+        }
+    }
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Pair<string, int> pair = new Pair<string, int>("Hello", 12);
+        Console.WriteLine($"A = {pair.firstValue}, B = {pair.secondValue}");
+        Console.ReadLine();
+
     }
 }
